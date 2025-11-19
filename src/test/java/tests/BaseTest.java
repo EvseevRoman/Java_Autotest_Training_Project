ced50d48729f783616a31445fe5dc850962d6319
Evseev Roman <evseeff.roman@mail.ru>
2025-11-19 13:56:40 +0300

    changed pom file, added dependencies, chenged file BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,14 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.*;
 
 import java.time.Duration;
@@ -15,20 +19,28 @@ public class BaseTest {
     ProductsPage productsPage;
     CartPage cartPage;
 
+    @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("--guest");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        loginPage = new LoginPage(driver);
-        productsPage = new ProductsPage(driver);
-        cartPage = new CartPage(driver);
-    }
+    public void setUp(@Optional("chrome") String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            options.addArguments("--guest");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver();
+            driver = new EdgeDriver();
+        }
 
-    @AfterMethod(alwaysRun = true)
-    public void close() {
-        driver.quit();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+            loginPage = new LoginPage(driver);
+            productsPage = new ProductsPage(driver);
+            cartPage = new CartPage(driver);
+        }
+
+        @AfterMethod(alwaysRun = true)
+        public void close () {
+            driver.quit();
+        }
     }
-}
